321a6c144a18ed8070d7a1076def78ae4ba46e72
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-11-18 20:51:36 +1100

    feat(App): enable fetching
    did this in a useEffect just for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
-const App = () => <></>;
+import { useEffect } from "react";
+
+const App = () => {
+  useEffect(() => {
+    // todo: might need to fetch data like the docs to clean up effect: https://react.dev/learn/synchronizing-with-effects#fetching-data
+    fetch("https://pokeapi.co/api/v2/pokemon/69", { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error: unknown) => {
+        console.error(error);
+      });
+  }, []);
+
+  return <></>;
+};
 
 export default App;
