7839286295aa5499a336eee392e75b75853710a6
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-11-18 22:57:25 +1100

    refactor(useAllPokemon): add void keyword
    this just silences the no-floating-promises linter error

--- src/hooks/useAllPokemons.ts
+++ src/hooks/useAllPokemons.ts
@@ -68,7 +68,7 @@ export const useAllPokemons = (pokemonIds: number[]) => {
       }
     };
 
-    fetchAllPokemons(pokemonIds);
+    void fetchAllPokemons(pokemonIds);
 
     return () => {
       shouldIgnore = true;
