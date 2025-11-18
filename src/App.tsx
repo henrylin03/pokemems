4ed414055cc996cec3f99787f42b1885b6e470ff
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-11-18 20:57:12 +1100

    feat(App): handle error based on response's status code

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { useEffect } from "react";
 const App = () => {
   useEffect(() => {
     // todo: might need to fetch data like the docs to clean up effect: https://react.dev/learn/synchronizing-with-effects#fetching-data
+    // todo: refactor to use async/await
     fetch("https://pokeapi.co/api/v2/pokemon/69", { mode: "cors" })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status >= 400)
+          throw new Error("error when retrieving Pokemon data from PokeAPI");
+        return response.json();
+      })
       .then((response) => {
         console.log(response);
       })
