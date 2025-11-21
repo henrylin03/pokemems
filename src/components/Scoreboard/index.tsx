8215a5fe774c8e5dd5f9aa436e4fc03431f5ceaf
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-11-21 13:55:04 +1100

    feat(Scoreboard): style text

--- src/components/Scoreboard/index.tsx
+++ src/components/Scoreboard/index.tsx
@@ -1,10 +1,16 @@
 import styles from "./Scoreboard.module.css";
 
 const Scoreboard = () => (
-  <section>
+  <section className={styles.section}>
     <ul className={styles.scores}>
-      <li className={styles.scoreItem}>score: 0</li>
-      <li className={styles.scoreItem}>high score: 0</li>
+      <li className={styles.scoreItem}>
+        <p className={styles.label}>score:</p>
+        <p className={styles.score}>0</p>
+      </li>
+      <li className={styles.scoreItem}>
+        <p className={styles.label}>high score:</p>
+        <p className={styles.score}>0</p>
+      </li>
     </ul>
   </section>
 );
