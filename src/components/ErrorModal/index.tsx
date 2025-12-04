171aa24d5878e32a7c8d18da3e406681ded18536
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-04 18:11:32 +1100

    feat(ErrorModal): layout modal, now that there's an image. adjust line-heights and margins

--- src/components/ErrorModal/index.tsx
+++ src/components/ErrorModal/index.tsx
@@ -6,34 +6,40 @@ interface Props {
   error: string;
 }
 
-const ErrorModal = ({ error }: Props) => (
-  <div className={styles.backdrop}>
-    <article className={styles.modal}>
-      <img
-        src={img}
-        className={styles.img}
-        alt="Confused Psyduck"
-        loading="lazy"
-      />
-      <h1 className={styles.heading}>Uh oh!</h1>
-      <p className={styles.copy}>
-        There's been an issue retrieving the Pokémons.
-        <br /> Please check your internet connection and try again.
-      </p>
-      <ul className={styles.buttons}>
-        <li>
-          <Button onClick={() => {}} className={styles.button}>
-            Try again
-          </Button>
-        </li>
-        <li>
-          <Button onClick={() => {}} className={styles.button}>
-            Report issue
-          </Button>
-        </li>
-      </ul>
-    </article>
-  </div>
-);
+const ErrorModal = ({ error }: Props) => {
+  console.error(error);
+
+  return (
+    <div className={styles.backdrop}>
+      <article className={styles.modal}>
+        <img
+          src={img}
+          className={styles.img}
+          alt="Confused Psyduck"
+          loading="lazy"
+        />
+        <div className={styles.text}>
+          <h1 className={styles.heading}>Uh oh!</h1>
+          <p className={styles.copy}>
+            There's been an issue retrieving the Pokémons.
+            <br /> Please check your internet connection and try again.
+          </p>
+        </div>
+        <ul className={styles.buttons}>
+          <li>
+            <Button onClick={() => {}} className={styles.button}>
+              Try again
+            </Button>
+          </li>
+          <li>
+            <Button onClick={() => {}} className={styles.button}>
+              Report issue
+            </Button>
+          </li>
+        </ul>
+      </article>
+    </div>
+  );
+};
 
 export default ErrorModal;
