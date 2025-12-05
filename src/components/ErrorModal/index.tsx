83c3dacdf11db53e351c4fc95c5313c7599fb56e
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-05 17:53:06 +1100

    feat(ErrorModal): restore styling

--- src/components/ErrorModal/index.tsx
+++ src/components/ErrorModal/index.tsx
@@ -1,6 +1,7 @@
 import Button from "../ui/Button";
 import Modal from "../ui/Modal";
 import img from "./confused-psyduck.png";
+import styles from "./ErrorModal.module.css";
 
 interface Props {
   errorMessage: string;
@@ -13,19 +14,26 @@ const LINK_TO_NEW_GITHUB_ISSUE =
 const ErrorModal = ({ errorMessage, retryFetch }: Props) => {
   console.error("Error:", errorMessage);
   return (
-    <Modal shouldShow={Boolean(errorMessage)}>
-      <img src={img} alt="Confused Psyduck" loading="lazy" />
+    <Modal shouldShow={Boolean(errorMessage)} className={styles.modal}>
+      <img
+        src={img}
+        alt="Confused Psyduck"
+        loading="lazy"
+        className={styles.img}
+      />
       <div>
-        <h1>Uh oh!</h1>
-        <p>
+        <h1 className={styles.heading}>Uh oh!</h1>
+        <p className={styles.copy}>
           There's been an issue retrieving the Pokémons.
           <br /> Please check your internet connection and try again.
         </p>
       </div>
-      <ul>
+      <ul className={styles.buttons}>
         <li>
           {/* TODO: fix type error here */}
-          <Button onClick={retryFetch}>Try again</Button>
+          <Button onClick={retryFetch} className={styles.button}>
+            Try again
+          </Button>
         </li>
         <li>
           <a
@@ -33,6 +41,7 @@ const ErrorModal = ({ errorMessage, retryFetch }: Props) => {
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Report issue on GitHub"
+            className={styles.button}
           >
             <Button>Report issue</Button>
           </a>
