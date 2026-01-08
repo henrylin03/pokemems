f0c5fe2911ce48033c559de64bb598c6752b6c23
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-01-08 11:54:09 +1100

    test(playwright.config): run local devserver before running tests, and test on mobile viewports too
    ensures functionality on smaller screens (might not test UI responsiveness using e2e because might be too brittle and leads to high maintenance). i think running on local devserver is better rather than in prod (as I had done in PR #63, which I'll sack).

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,14 +50,14 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
 
     /* Test against branded browsers. */
     // {
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:5173", // default for Vite: https://vite.dev/guide/#manual-installation
+    reuseExistingServer: !process.env.CI,
+  },
 });
